76dfe33b85b3f8b9d3c1a3eb3785d6e719f24df0
Diogo Alves <diogo.alves@ancine.gov.br>
2017-11-10 17:34:03 -0200

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import ListConstacts from './ListContacts';
+import CreateContact from './CreateContact';
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
@@ -19,16 +21,34 @@ class App extends Component {
       this.setState( (currentState) => ({
         contacts: currentState.contacts.filter((c) => c.id !== contact.id)
       }));
-    })  
+    })
+  }
+
+  createContact = (contact) => {
+    ContactsAPI.create(contact).then( (contact) => {
+      this.setState( (currentState) => ({
+        contacts: currentState.contacts.concat([ contact ])
+      }));
+    });
   }
 
   render() {
     return (
-      <div>
-        <ListConstacts 
-          onDeleteContact={this.removeContact} 
-          contacts={this.state.contacts} 
-        />
+      <div className='app'>
+        <Route exact path='/' render={ () => (
+          <ListConstacts
+            onDeleteContact={this.removeContact}
+            contacts={this.state.contacts}
+          />
+        )} />
+        <Route path='/create' render={ ({ history }) => (
+          <CreateContact
+            onCreateContact={ (contact) => {
+              this.createContact(contact);
+              history.push('/');
+            }}
+          />
+        )}/>
       </div>
     )
   }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by';
@@ -39,14 +40,18 @@ class ListContacts extends Component {
 
     return (
       <div className='list-contacts'>
-        <div className='list-contacs-top'>
+        <div className='list-contacts-top'>
           <input
             className='search-contacts'
             type='text'
             placeholder='Search contacts'
             value={query}
-            onChange={ (event) => this.updateQuery(event.target.value) }
+            onChange={(event) => this.updateQuery(event.target.value)}
           />
+          <Link
+            to='/create'
+            className='add-contact'
+          >Add Contact</Link>
         </div>
 
         { showingContacts.length !== contacts.length && (
